ebbdf25dca35d9b7d9fd77a18ebc02c3042dd25d
kashyap-sharma <kashyap.sharma@jlabs.co>
2017-02-12 16:04:57 +0530

    test

--- app/src/main/java/co/jlabs/xar/activity_area/ArtistDetails.java
+++ app/src/main/java/co/jlabs/xar/activity_area/ArtistDetails.java
@@ -1,15 +1,23 @@
 package co.jlabs.xar.activity_area;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
 
 import co.jlabs.xar.R;
 
 public class ArtistDetails extends AppCompatActivity {
 
+    private ConstraintLayout activity_artist_details;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artist_details);
+        initView();
+    }
+
+    private void initView() {
+        activity_artist_details = (ConstraintLayout) findViewById(R.id.activity_artist_details);
     }
 }
